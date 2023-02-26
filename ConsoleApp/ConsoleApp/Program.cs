f77ad000a4cf2190d63fb05f1cc210ca4531d76f
Michał Zimniak <zimniakm@gmail.com>
2023-02-26 15:40:52 +0100

    Zadanie dzień 4.

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+string sex = "Kobieta";
+int age = 25;
+
+if (name == "Ewa")
+{if (sex == "Kobieta")
+
+    if (age < 30)
+    {
+        Console.WriteLine("Ewa, poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Ewa, powyżej 30 lat");
+    }
+else
+    Console.WriteLine("To mężczyzna");
+}
+else 
+{
+    Console.WriteLine("To nie Ewa");
+};
+
