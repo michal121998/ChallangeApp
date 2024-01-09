3e36e9dbc8d97c5aebcedc2e308209573cb08104
Michał Zimniak <zimniakm@gmail.com>
2024-01-09 19:50:46 +0100

    Zadanie dzień 4.

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string imię = "Michał";
+string płeć = "m";
+int wiek = 25;
+
+if (płeć == "k")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (imię == "Ewa" && wiek == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (płeć == "m" && wiek < 18 )
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
