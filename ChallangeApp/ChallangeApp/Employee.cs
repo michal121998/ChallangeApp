93d5ec04c2d4092af437a1d71726dcf06a41e7f0
Michał Zimniak <zimniakm@gmail.com>
2024-01-12 22:40:12 +0100

    Zadanie dzień 6

---
+++ ChallangeApp/ChallangeApp/Employee.cs
@@ -0,0 +1,32 @@
+﻿namespace ChallangeApp
+{
+    public class Employee
+    {
+        private List<int> point = new List<int>();
+        public Employee(string name, string lastName, int age)
+        {
+            this.Name = name;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public int maxResult
+        {
+            get
+            {
+                return this.point.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            this.point.Add(number);
+        }
+    }
+}
--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,70 +1,38 @@
-﻿int number = 4566;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToArray();
+﻿using ChallangeApp;
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+Employee employee1 = new Employee("Anna", "Bujak", 20);
+Employee employee2 = new Employee("Adam", "Nowak", 24);
+Employee employee3 = new Employee("Angelika", "Gaj", 25);
 
-foreach (char letter in letters)
+employee1.AddScore(3);
+employee1.AddScore(6);
+employee1.AddScore(1);
+
+employee2.AddScore(9);
+employee2.AddScore(1);
+employee2.AddScore(3);
+
+employee3.AddScore(6);
+employee3.AddScore(4);
+employee3.AddScore(5);
+
+List<Employee> employees = new List<Employee>()
 {
-    if (letter == '0')
+    employee1, employee2, employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach(var employee in employees)
+{
+    if(employee.maxResult > maxResult)
     {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-        {
-        counter9++;
+        employeeWithMaxResult = employee;
+        maxResult = employee.maxResult;
     }
 }
 
-Console.WriteLine("Wyniki dla liczby:" + number);
-Console.WriteLine("0 = " + counter0);
-Console.WriteLine("1 = " + counter1);
-Console.WriteLine("2 = " + counter2);
-Console.WriteLine("3 = " + counter3);
-Console.WriteLine("4 = " + counter4);
-Console.WriteLine("5 = " + counter5);
-Console.WriteLine("6 = " + counter6);
-Console.WriteLine("7 = " + counter7);
-Console.WriteLine("8 = " + counter8);
-Console.WriteLine("9 = " + counter9);
+Console.WriteLine("Najlepszy pracownik to: " + employeeWithMaxResult.Name + " " + 
+    employeeWithMaxResult.LastName + " " + employeeWithMaxResult.Age +
+    " lat" + " z wynikiem" + " " + employeeWithMaxResult.maxResult  );
